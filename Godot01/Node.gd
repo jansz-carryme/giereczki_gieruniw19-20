8dc12e042006b252a0a18bcc983f7a7595f63804
jansz-carryme <72602698+jansz-carryme@users.noreply.github.com>
2020-10-23 11:34:21 +0200

    zabawa dalsza

--- Godot01/Node.gd
+++ Godot01/Node.gd
@@ -7,14 +7,19 @@ var nameSecond = "Thorhamptonington"
 var dada = 2
 
 var names = ["Jan", "John", "Konar", "Seba", "Sztywny", "Gonsalito"]
-var welcomes = ["Czesc", "Hello", "No hejo", "Siema"]
+var welcomes = ["Czesc", "Hello", "No hejo", "Siema", "Elo"]
+var coLubieRobic = ["Wziuu", "Łiiiii", "Aaaaa", "BRRRRR", "REEEEE"]
 
 func _ready():
 # print("Hello World")
 # print (2+7)
 # print ("2+7")
-	print  (welcomeSecond + " Mon nom est " + nameFirst) 
-	print  (welcomes [1] + " my name is " + names [2])
+
+	print (welcomes [3] + "!" + " Mam na imię " + names [0] + "." + " Lubię robić " + coLubieRobic [1]) 
+	print (welcomes [0] + "!" + " Mam na imię " + names [1] + "." + " Lubię robić " + coLubieRobic [3]) 
+	print (welcomes [1] + "!" + " Mam na imię " + names [3] + "." + " Lubię robić " + coLubieRobic [4]) 
+	print (welcomes [4] + "!" + " Mam na imię " + names [2] + "." + " Lubię robić " + coLubieRobic [0]) 
+	print (welcomes [2] + "!" + " Mam na imię " + names [4] + "." + " Lubię robić " + coLubieRobic [2]) 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
