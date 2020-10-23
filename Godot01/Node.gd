c808157a88668e14e547f61fccde196dec62e248
jansz-carryme <72602698+jansz-carryme@users.noreply.github.com>
2020-10-23 11:08:05 +0200

    Pierwsze zajęcia z Godota
    W sumie to fajne to

---
+++ Godot01/Node.gd
@@ -0,0 +1,21 @@
+extends Node
+
+var welcomeFirst = "Hello hello"
+var welcomeSecond = "Bonjour"
+var nameFirst = "Gonsalito"
+var nameSecond = "Thorhamptonington"
+var dada = 2
+
+var names = ["Jan", "John", "Konar", "Seba", "Sztywny", "Gonsalito"]
+var welcomes = ["Czesc", "Hello", "No hejo", "Siema"]
+
+func _ready():
+# print("Hello World")
+# print (2+7)
+# print ("2+7")
+	print  (welcomeSecond + " Mon nom est " + nameFirst) 
+	print  (welcomes [1] + " my name is " + names [2])
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
