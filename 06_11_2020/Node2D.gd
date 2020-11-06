9a01b42d09155175c4c8f14017ce7683883cd0d4
jansz-carryme <72602698+jansz-carryme@users.noreply.github.com>
2020-11-06 10:02:13 +0100

    Create Node2D.gd

---
+++ 06_11_2020/Node2D.gd
@@ -0,0 +1,31 @@
+extends Node
+
+var welcomeFirst = "Hello hello"
+var welcomeSecond = "Bonjour"
+var nameFirst = "Gonsalito"
+var nameSecond = "Thorhamptonington"
+var dada = 2
+
+var names = ["Jan", "John", "Konar", "Seba", "Sztywny", "Gonsalito"]
+var welcomes = ["Czesc", "Hello", "No hejo", "Siema", "Elo"]
+var coLubieRobic = ["Wziuu", "Łiiiii", "Aaaaa", "BRRRRR", "REEEEE"]
+
+func _ready():
+# print("Hello World")
+# print (2+7)
+# print ("2+7")
+
+	print (welcomes [3] + "!" + " Mam na imię " + names [0] + "." + " Lubię robić " + coLubieRobic [1]) 
+	print (welcomes [0] + "!" + " Mam na imię " + names [1] + "." + " Lubię robić " + coLubieRobic [3]) 
+	print (welcomes [1] + "!" + " Mam na imię " + names [3] + "." + " Lubię robić " + coLubieRobic [4]) 
+	print (welcomes [4] + "!" + " Mam na imię " + names [2] + "." + " Lubię robić " + coLubieRobic [0]) 
+	print (welcomes [2] + "!" + " Mam na imię " + names [4] + "." + " Lubię robić " + coLubieRobic [2]) 
+
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
