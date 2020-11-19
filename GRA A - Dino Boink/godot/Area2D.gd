a912886bc5d9f6effb5b2382058c77385ba5cdad
jansz-carryme <72602698+jansz-carryme@users.noreply.github.com>
2020-11-19 20:04:44 +0100

    gra Dino Boink - prjekt 1

---
+++ GRA A - Dino Boink/godot/Area2D.gd
@@ -0,0 +1,7 @@
+extends Area2D
+
+func _ready():
+	pass
+
+func _on_Area2D_body_entered(body):
+	get_tree().reload_current_scene()
---
+++ GRA A - Dino Boink/godot/level.gd
@@ -0,0 +1,6 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+
