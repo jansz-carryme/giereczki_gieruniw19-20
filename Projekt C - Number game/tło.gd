957a2830ec0e96b4e39730a9298f59853be2540a
jansz-carryme <72602698+jansz-carryme@users.noreply.github.com>
2021-01-17 12:16:54 +0100

    update next day - zerowanie wartosci po wpisie gracza
    17.01

--- Projekt C - Number game/tło.gd
+++ Projekt C - Number game/tło.gd
@@ -16,7 +16,6 @@ func _ready():
 	print(my_random_number)
 	
 	
-	
 	#print(number_txt)
 	get_node("LabelTries").value = tries
 ##wersja string##
@@ -26,10 +25,10 @@ func _ready():
 #gdy gracz klika enterem w SpinBoxa
 func _on_SpinBoxUI_value_changed(try):
 	#if Input.is_action_just_pressed('ui_accept'):
-	tries = tries - 1
+	tries = tries - 0.5
 	get_node("LabelTries").value = tries
 	if try == $SpinBoxCode.value:
-		get_tree().change_scene("res://close oen.tscn")
+		get_tree().change_scene("res://win.tscn")
 	if try > $SpinBoxCode.value:
 		get_node("LabelUI").text = "You critically overestimate!"
 	if try < $SpinBoxCode.value:
@@ -39,4 +38,5 @@ func _on_SpinBoxUI_value_changed(try):
 #gdy jest 0 prób i wygrana
 	if $LabelTries.value == 0 and try == $SpinBoxCode.value:
 		get_tree().change_scene("res://win.tscn")
-		
+	#reset wartości do zera
+	get_node("SpinBoxUI").value = 0
